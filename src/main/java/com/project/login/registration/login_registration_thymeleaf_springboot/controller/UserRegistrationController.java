888992b6edee6727d7c2882d2bfbe687c18ad6d1
samuel <samuel14sromovsky@gmail.com>
2022-06-17 00:43:06 +0200

    created packages service, web, controller and files

--- src/main/java/com/project/login/registration/login_registration_thymeleaf_springboot/controller/UserRegistrationController.java
+++ src/main/java/com/project/login/registration/login_registration_thymeleaf_springboot/controller/UserRegistrationController.java
@@ -1,10 +1,19 @@
 package com.project.login.registration.login_registration_thymeleaf_springboot.controller;
 
 
+import com.project.login.registration.login_registration_thymeleaf_springboot.service.UserService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/registration")
 public class UserRegistrationController {
 
+    private UserService userService;
 
+    //constructor
+    public UserRegistrationController(UserService userService) {
+        super();
+        this.userService = userService;
+    }
 }
